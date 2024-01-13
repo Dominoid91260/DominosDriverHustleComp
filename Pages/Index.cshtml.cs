a2c3a771bc7657923dab02b6785d2e2d836859f8
Tyler Duffus <tyled94@hotmail.com>
2024-01-14 00:38:47 +1000

    Added ranking table to index page

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,20 +1,52 @@
+using DominosDriverHustleComp.Data;
+
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace DominosDriverHustleComp.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
+        public class DriverViewModel
         {
-            _logger = logger;
+            // Used for "Average" result
+            public bool IsSpecialResult = false;
+
+            public required string Name { get; set; }
+            public int NumDels { get; set; }
+            public TimeSpan AvgHustleOut { get; set; }
+            public TimeSpan AvgHustleIn { get; set; }
+            public TimeSpan AvgHustleCombined { get => AvgHustleIn + AvgHustleOut; }
         }
 
-        public void OnGet()
-        {
+        private readonly HustleCompContext _context;
+        public IEnumerable<DriverViewModel>? Drivers;
 
+        public IndexModel(HustleCompContext context)
+        {
+            _context = context;
+        }
+
+        public void OnGet(DateTime? WeekEnding)
+        {
+            if (WeekEnding.HasValue)
+            {
+                var wkEnding = WeekEnding.Value;
+
+                Drivers = _context.Deliveries
+                    .Include(d => d.Driver)
+                    .AsEnumerable()
+                    .Where(d => d.DispatchedAt <= wkEnding && (wkEnding - d.DispatchedAt).TotalDays <= 7)
+                    .GroupBy(d => d.Driver)
+                    .Select(g => new DriverViewModel()
+                    {
+                        Name = g.Key.FirstName + " " + g.Key.LastName,
+                        NumDels = g.Count(),
+                        AvgHustleOut = TimeSpan.FromSeconds(Math.Floor(g.Select(d => (d.LeftStoreAt - d.DispatchedAt).TotalSeconds).Average())),
+                        AvgHustleIn = TimeSpan.FromSeconds(Math.Floor(g.Select(d => (d.InAt - d.StoreEnteredAt).TotalSeconds).Average()))
+                    });
+            }
         }
     }
 }
