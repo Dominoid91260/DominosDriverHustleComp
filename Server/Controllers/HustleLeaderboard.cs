b2913201b627fb07b66b52328d85b24bba388446
Dominoid <170596367+Dominoid91260@users.noreply.github.com>
2024-06-18 09:43:17 +1000

    Added leaderboard to homepage

---
+++ Server/Controllers/HustleLeaderboard.cs
@@ -0,0 +1,25 @@
+﻿using DominosDriverHustleComp.Server.Services;
+using DominosDriverHustleComp.Shared.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DominosDriverHustleComp.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HustleLeaderboard : ControllerBase
+    {
+        private readonly HustleTracker _hustleTracker;
+
+        public HustleLeaderboard(HustleTracker hustleTracker)
+        {
+            _hustleTracker = hustleTracker;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return new JsonResult(_hustleTracker.GetLeaderboardData());
+        }
+    }
+}
