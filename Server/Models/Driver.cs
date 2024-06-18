bad977bb92bda4371df91ee13497408671102527
Dominoid <170596367+Dominoid91260@users.noreply.github.com>
2024-06-18 15:31:55 +1000

    Added Driver navigation properties

--- Server/Models/Driver.cs
+++ Server/Models/Driver.cs
@@ -5,5 +5,8 @@
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+
+        public virtual IEnumerable<Delivery> Deliveries { get; set; }
+        public virtual IEnumerable<DeliverySummary> WeeklySummaries { get; set; }
     }
 }
