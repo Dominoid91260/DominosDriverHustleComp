95a60a0528a19586fc6e0b9f6364072ca105db4e
Dominoid <170596367+Dominoid91260@users.noreply.github.com>
2024-06-19 22:23:40 +1000

    Added `WeekEnding` index to `DeliverySummary` model

---
+++ Server/Migrations/20240619121815_Added-DeliverySummary-Indexes.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DominosDriverHustleComp.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedDeliverySummaryIndexes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_DeliverySummaries_WeekEnding",
+                table: "DeliverySummaries",
+                column: "WeekEnding");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_DeliverySummaries_WeekEnding",
+                table: "DeliverySummaries");
+        }
+    }
+}
