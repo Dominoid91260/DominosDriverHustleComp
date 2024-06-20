5c710010ff3a8b77e928a0d922a4a9b1d3a84747
Dominoid <170596367+Dominoid91260@users.noreply.github.com>
2024-06-20 17:54:15 +1000

    Added Reports page

---
+++ Server/Controllers/ReportsController.cs
@@ -0,0 +1,23 @@
+﻿using DominosDriverHustleComp.Server.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DominosDriverHustleComp.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportsController : ControllerBase
+    {
+        private readonly HustleCompContext _context;
+
+        public ReportsController(HustleCompContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<DateTime>> Get()
+        {
+            return _context.WeeklySummaries.Select(ws => ws.WeekEnding);
+        }
+    }
+}
