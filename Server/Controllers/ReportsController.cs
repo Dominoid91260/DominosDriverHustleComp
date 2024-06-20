1b403cee91fef0f4caa5006dc83e9c632b29e70d
Dominoid <170596367+Dominoid91260@users.noreply.github.com>
2024-06-20 23:09:45 +1000

    Added win and outlier streaks

--- Server/Controllers/ReportsController.cs
+++ Server/Controllers/ReportsController.cs
@@ -10,6 +10,18 @@ namespace DominosDriverHustleComp.Server.Controllers
     [ApiController]
     public class ReportsController : ControllerBase
     {
+        private struct Streaks
+        {
+            public int WinStreak { get; set; }
+            public int OutlierStreak { get; set; }
+
+            public Streaks()
+            {
+                WinStreak = 0;
+                OutlierStreak = 0;
+            }
+        }
+
         private readonly HustleCompContext _context;
 
         public ReportsController(HustleCompContext context)
@@ -35,12 +47,19 @@ namespace DominosDriverHustleComp.Server.Controllers
             if (weeklySummary is null)
                 return [];
 
-            return weeklySummary.DeliverySummaries.Select(ds => new ReportRecord
+            return weeklySummary.DeliverySummaries.Select(ds =>
             {
-                Name = ds.Driver.FirstName + " " + ds.Driver.LastName,
-                AvgOut = ds.AvgHustleOut,
-                AvgIn = ds.AvgHustleIn,
-                PreviousWeekStats = GetPreviousStatsForDriver(ds.Driver, ds.WeekEnding)
+                var streaks = GetStreaksForDriver(ds.Driver, ds.WeekEnding);
+
+                return new ReportRecord
+                {
+                    Name = ds.Driver.FirstName + " " + ds.Driver.LastName,
+                    AvgOut = ds.AvgHustleOut,
+                    AvgIn = ds.AvgHustleIn,
+                    PreviousWeekStats = GetPreviousStatsForDriver(ds.Driver, ds.WeekEnding),
+                    WinStreak = streaks.WinStreak,
+                    Outlier = streaks.OutlierStreak
+                };
             });
         }
 
@@ -60,6 +79,64 @@ namespace DominosDriverHustleComp.Server.Controllers
             {
                 AvgIn = lastWeek.AvgHustleIn,
                 AvgOut = lastWeek.AvgHustleOut
+            };
+        }
+
+        private Streaks GetStreaksForDriver(Driver driver, DateTime from)
+        {
+            if (!driver.WeeklySummaries.Any())
+                return default;
+
+            var currentDelSummary = driver.WeeklySummaries.First(ds => ds.WeekEnding == from);
+            var currentWeeklySummary = _context.WeeklySummaries.Find(from);
+
+            ///@TODO make these configurable
+            const float benchmarkSeconds = 160; // this is combined benchmark so make sure its 2x in/out
+            const float outlier = 10;
+
+            // Include the requested week in the search, this way a 1 week win will be 1 streak.
+            // We can handle displaying this on the client by checking if the streak is 1
+            var delSummaries = driver.WeeklySummaries.Where(ds => ds.WeekEnding <= from);
+
+            if (!delSummaries.Any())
+                return default;
+
+            bool bCountingWins = currentDelSummary.AvgHustleCombined < benchmarkSeconds;
+            int wins = 0;
+
+            bool bCountingOutliers = currentDelSummary.AvgHustleCombined >= currentWeeklySummary.AvgHustleCombined + outlier;
+            int outliers = 0;
+
+            foreach (var ds in delSummaries.OrderByDescending(ds => ds.WeekEnding))
+            {
+                var weeklySummary = _context.WeeklySummaries.Find(ds.WeekEnding);
+
+                if (ds.AvgHustleCombined >= benchmarkSeconds)
+                {
+                    bCountingWins = false;
+                }
+                else if (bCountingWins)
+                {
+                    wins++;
+                }
+
+                if (ds.AvgHustleCombined < weeklySummary.AvgHustleCombined + outlier)
+                {
+                    bCountingOutliers = false;
+                }
+                else if (bCountingOutliers)
+                {
+                    outliers++;
+                }
+
+                if (!bCountingWins && !bCountingOutliers)
+                    break;
+            }
+
+            return new Streaks
+            {
+                WinStreak = wins,
+                OutlierStreak = outliers,
             };
         }
     }
