8c72a8501cf8676ff3ded7d3da622672e1065464
Dominoid <170596367+Dominoid91260@users.noreply.github.com>
2024-06-23 23:01:30 +1000

    Added driver disqualification modal

---
+++ Server/Controllers/DriversController.cs
@@ -0,0 +1,43 @@
+﻿using DominosDriverHustleComp.Server.Data;
+using DominosDriverHustleComp.Shared.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DominosDriverHustleComp.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DriversController : ControllerBase
+    {
+        private readonly HustleCompContext _context;
+
+        public DriversController(HustleCompContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("Disqualifications")]
+        public async Task<IEnumerable<DriverDisqualification>> GetDisqualifications()
+        {
+            return _context.Drivers.Select(d => new DriverDisqualification
+            {
+                DriverId = d.Id,
+                Name = d.FirstName + " " + d.LastName,
+                IsDisqualified = d.IsPermanentlyDisqualified
+            });
+        }
+
+        [HttpPost("Disqualifications/{driverId}")]
+        public async Task<IActionResult> PostDisqualifications(int driverId, [FromBody] bool isDisqualified)
+        {
+            var driver = _context.Drivers.Find(driverId);
+
+            if (driver is null)
+                return NotFound();
+
+            driver.IsPermanentlyDisqualified = isDisqualified;
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+    }
+}
---
+++ Shared/ViewModels/DriverDisqualification.cs
@@ -0,0 +1,9 @@
+﻿namespace DominosDriverHustleComp.Shared.ViewModels
+{
+    public class DriverDisqualification
+    {
+        public int DriverId { get; set; }
+        public string Name { get; set; }
+        public bool IsDisqualified { get; set; }
+    }
+}
